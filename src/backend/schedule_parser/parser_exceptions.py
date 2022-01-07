39dd575b9f9081fc9099f6ab4d5f0a9d02be3afb
Sujal Singh <email.sujalsingh@gmail.com>
2022-01-07 16:10:56 +0530

    DELETE: taking a new approach to schedule parser

--- src/backend/schedule_parser/parser_exceptions.py
+++
@@ -1,8 +0,0 @@
-class MissingParserTypeParseError(Exception):
-    def __init__(self):
-        super().__init__("Raw config lacks a type attribute")
-
-
-class InvalidParserTypeParseError(Exception):
-    def __init__(self):
-        super().__init__("Raw config with invalid parser type. Type attribute has to be one of 'daily' or 'weekend'")
